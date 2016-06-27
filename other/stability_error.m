d4876eb0b7de197c99b31ce0fd918df22c748b79
Gregory Ditzler <gregory.ditzler@gmail.com>
2016-06-27 19:59:01 -0400

    updated stability

--- other/stability_error.m
+++ other/stability_error.m
@@ -1,3 +1,3 @@
 function err = stability_error(Aopt, Aest, p)
-err = numel(intersect(Aopt, Aest))/(p-numel(Aopt));
-
+% err = numel(intersect(Aopt, Aest))/(p-numel(Aopt));
+err = numel(intersect(Aopt, Aest))/numel(union(Aopt, Aest));
