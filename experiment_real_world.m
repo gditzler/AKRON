1de04e786d86c1ebf10e822f72eda6b1c1548495
Gregory Ditzler <gregory.ditzler@gmail.com>
2016-07-07 11:24:09 -0400

    updated

--- experiment_real_world.m
+++ experiment_real_world.m
@@ -5,7 +5,7 @@ close all
 addpath akron/
 addpath other/
 
-n_cv = 10;
+n_cv = 5;
 k_alg = 15;
 
 [A, x, y] = cs_model(200, 250, 8, 'Gaussian');
@@ -46,24 +46,9 @@ A = data(:, 1:end-1);
 y = data(:, end);
 y(y==0) = -1;
 errors(end+1, :) = run_cv(A, y, n_cv, k_alg, classification);
-%% echocardiogram_test
-A=load('~/Downloads/ARCENE/ARCENE/arcene_train.data');
-y=load('~/Downloads/ARCENE/ARCENE/arcene_train.labels');
-
-stdX = std(A);                            %stdX = standard deviation of X
-idx1 = stdX~=0;                           %idx1 persists where the std X is not zero
-centrX = A-repmat(mean(A),size(A,1),1);   %Subtracts a tiling from X
-A(:,idx1) = centrX(:,idx1)./repmat(stdX(:,idx1),size(A,1),1); %$X at the specified indices = centrX at the same indices, divided by a tiled matrix of the standard deviation at the specified indices
-
-A = (A-repmat(mean(A),size(A,1),1));%./repmat(std(A),size(A,1),1); %Overwrite X as current X-tiled mean
-A = A./repmat(sqrt(sum(A.*A,2)),1, size(A,2));    %Overwrite X as x X divided by tiled sqrt(sum of X.*X,2)
-
-
-k_alg = floor(.05*size(A,2));
-y(y==0) = -1;
-errors(end+1, :) = run_cv(A, y, n_cv, k_alg, classification);
 
 %%
+% CoSaMP, OMP, AKRON, 
 errors(isnan(errors)) = 100;
 [hZtest, pZtest, pFtest] = friedman_demsar(errors, 'two', .1)
 ranks = rank_rows(errors);
