6f2d398627ccdfc71c4e9e22610db4792868e2a1
Gregory Ditzler <ditzler@email.arizona.edu>
2016-07-12 13:57:22 -0700

    updated real work

--- experiment_real_world.m
+++ experiment_real_world.m
@@ -8,11 +8,10 @@ addpath other/
 n_cv = 5;
 k_alg = 7;
 base_path = '/scratch/ditzler/Git/ClassificationDatasets/csv/';
-dsets = {'echocardiogram_test.csv'
-  'breast-cancer-wisc-prog_test.csv'
+dsets = {'breast-cancer-wisc-prog_test.csv'
   'conn-bench-sonar-mines-rocks_test.csv'
   'spectf_train_test.csv'
-  'csv/trains.csv'
+  'trains.csv'
   'musk-1_test.csv'};
 errors = zeros(length(dsets), 6);
 classification = 1;
@@ -20,15 +19,15 @@ classification = 1;
 delete(gcp('nocreate'));
 parpool(25);
 for n = 1:length(dsets)
-  try 
+  %try 
     data = load([base_path, dsets{n}]); 
     A = data(:, 1:end-1);
     y = data(:, end);
     y(y==0) = -1;
     errors(n, :) = run_cv(A, y, n_cv, k_alg, classification);
-  catch 
-    disp(['Error on ', dsets{n}]);
-  end
+  %catch 
+  %disp(['Error on ', dsets{n}]);
+  %end
 end
 
 delete(gcp);
