25921f9192fe3533a5c578056ae7b7bc200a4eff
Gregory Ditzler <gregory.ditzler@gmail.com>
2016-07-13 08:24:48 -0400

    init commit

---
+++ experiment_camera.m
@@ -0,0 +1,33 @@
+clc
+clear
+close all
+
+load ~/Downloads/l1magic/Data/camera.mat
+
+I = camera(81:112,37:68);
+n = 32;
+N = n*n;
+I = I/norm(I(:));
+I = I - mean(I(:));
+x = reshape(I,N,1);
+
+
+% num obs
+K = 300;
+
+% permutation P and observation set OMEGA
+P = randperm(N)';
+q = randperm(N/2-1)+1;
+OMEGA = q(1:K/2)';
+
+FT = 1/sqrt(N)*fft(eye(N));
+A = sqrt(2)*[real(FT(OMEGA,:)); imag(FT(OMEGA,:))];
+A = [1/sqrt(N)*ones(1,N); A];
+At = [];
+% observations
+b = A*x;
+% initial point
+x0 = A'*b;
+
+errors = run_cv(A, b, 5, 50, 0);
+
