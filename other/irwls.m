4f4ff483fda068426cf54dc6b02321002e304534
Gregory Ditzler <ditzler@email.arizona.edu>
2017-07-18 16:39:56 -0700

    updated

--- other/irwls.m
+++ other/irwls.m
@@ -2,6 +2,7 @@ function x = irwls(A, b, max_iter, epsilon)
 % https://web.stanford.edu/~boyd/papers/pdf/rwl1.pdf
 [~, p] = size(A);
 w = ones(p, 1);
+w_old = w;
 
 for i = 1:max_iter
   
@@ -15,6 +16,10 @@ for i = 1:max_iter
   cvx_end
   
   w = 1./(epsilon + abs(xhat));
+  if norm(w-w_old, 2) < 1e-6
+    break
+  end
+  w_old = w;
 end
 x = xhat;
 x(abs(x)<1e-3) = 0;
